6c71b4692730974e9ea191aa3d39809270267803
Nicolás Gómez <Nvandres079@gmail.com>
2024-05-06 13:39:21 -0500

    Add Header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Instagram-Clone",
-  description: "Instagram clone coded with our little hands... (Nextjs and TailwindCss)",
+  description: "Instagram clone coded with our little hands. (Nextjs and TailwindCss)",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header
+      className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'
+    >
+      <div
+        className='flex justify-between items-center max-w-6xl mx-auto'
+      >
+
+        {/* Logo */}
+
+        <Link href='/' className='hidden lg:inline-flex' >
+          <Image 
+            src='/Instagram_logo_black.webp'
+            width={96}
+            height={96}
+            alt='Instagram-logo'
+          />
+        </Link>
+
+        <Link href='/' className='lg:hidden' >
+          <Image 
+            src='/800px-Instagram_logo_2016.webp'
+            width={40}
+            height={40}
+            alt='Instagram-logo'
+          />
+        </Link>
+
+        {/* Search Input */}
+
+        <input 
+          type="text"
+          placeholder='Search'
+          className='bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]' 
+        />
+
+        {/* Menu Items */}
+
+        <button
+          className='text-sm font-semibold text-blue-500'
+        >Log in</button>
+      </div>
+    </header>
+  )
+}
